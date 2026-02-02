afda8aaa1b06082f20310529d32fe1e9ca020a29
Oluwatosin Paul Jegede <info@telewest.com.ng>
2026-02-02 17:50:59 +0100

    Apply IDApplication model changes

--- applications/signals.py
+++ applications/signals.py
@@ -1,18 +1,17 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from idcards.models import IDCard
 from .models import IDApplication
 from idcards.services import generate_id_card
 
 
 @receiver(post_save, sender=IDApplication)
 def auto_issue_id_card(sender, instance, created, **kwargs):
-    # Only issue on first creation
+    # Only run on creation
     if not created:
         return
 
-    # Safety check: do not issue twice
-    if IDCard.objects.filter(student=instance.student).exists():
+    # Only issue AFTER approval
+    if instance.status != IDApplication.STATUS_APPROVED:
         return
 
     generate_id_card(instance)
