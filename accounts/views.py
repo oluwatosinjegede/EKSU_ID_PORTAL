6701f7cc6e31be69f4d692925657c551293d4da9
Oluwatosin Paul Jegede <info@telewest.com.ng>
2026-02-02 20:27:29 +0100

    Resolving NameError issues

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from students.models import Student
