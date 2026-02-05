d19374a0309e119fd9c4709878f2a110c052cbde
Oluwatosin Paul Jegede <jegedeoluwatosin10@gmail.com>
2026-02-05 19:44:08 +0100

    delete applications/urls.py

--- config/urls.py
+++ config/urls.py
@@ -3,16 +3,20 @@ from django.urls import path, include
 from idcards.views import verify_id
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
 
     path("verify/<uuid:uid>/", verify_id, name="verify_id"),
 
-    path("", include("applications.urls")),
+    path('', include('idcards.urls')),
     path("accounts/", include("accounts.urls")),
     path("students/", include("students.urls")),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
+  
+
