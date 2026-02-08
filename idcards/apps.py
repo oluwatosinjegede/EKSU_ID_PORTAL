571c72d9ce185eed3decc60ad3da1a62c18b7d6b
Oluwatosin Paul Jegede <jegedeoluwatosin10@gmail.com>
2026-02-08 15:50:28 +0100

    Fix AppConfig import

--- idcards/apps.py
+++ idcards/apps.py
@@ -1,6 +1,10 @@
+from django.apps import AppConfig
+
+
 class IdcardsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "idcards"
 
     def ready(self):
+        # Import signals safely (no circular import)
         import idcards.signals  # noqa
